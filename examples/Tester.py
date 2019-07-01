8812289e2f7d39b2d5365b760fddd1217115514d
Nathan Schnitzer <nathanschnitzer587@gmail.com>
2019-06-30 21:03:14 -0400

    Testing something

--- examples/Tester.py
+++ examples/Tester.py
@@ -2,10 +2,16 @@
 
 from breezycreate2 import Robot
 import time
+import pygame
+import playsound
 
 
 # pip install -i https://test.pypi.org/simple/ BreezyCreate2
 
+def handle_events():
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_w and event.key == pygame.K_a:
+            print('wa')
 
 # Create a Create 2 -- Connect over serial
 bot = Robot()
@@ -23,10 +29,28 @@ time.sleep(1)
 bot.setTurnSpeed(0)
 
 # Report bumper hits and wall proximity for 30 sec
-start_time = time.time()
-while (time.time() - start_time) < 30:
-    print('Bumpers" ' + str(bot.getBumpers()) + '   Wall: ' + str(bot.getWallSensor()))
+#start_time = time.time()
+#while (time.time() - start_time) < 30:
+#    print('Bumpers" ' + str(bot.getBumpers()) + '   Wall: ' + str(bot.getWallSensor()))
 
+
+#Detect keypresses and hits
+while True:
+    #Check for hit detections
+    bumpers = bot.getBumpers()
+    if bumpers[0]:
+        bot.stop()
+        playsound()
+        bot.start()
+    elif bumpers[1]:
+        bot.stop()
+        playsound()
+        bot.start()
+    #configure walls later
+
+    #handle keypresses
+    handle_events()
+    
 
 # Shut down the bot
 bot.close()
